f09bc738af24074391e9d2152774ff7c96973079
Noushin <parisanoushin@gmail.com>
2018-05-30 03:28:26 +0000

    task1 and 2

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 /* Task 1 */
-
+var heading=document.getElementById('heading')
+console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
 
-
-
+function addtext2()
+{
+    var task2div=document.getElementById('task2a')
+    var paragraph =document.createElement('p')
+    paragraph.innerText="Hello world"
+    task2div.appendChild(paragraph)
+    
+}
+ 
+    var textBtn=document.getElementById('TextBtn2a')
+    textBtn.addEventListener('click',addtext2)
+    
+    //2b
+    function changeBGcolor(event){
+        var body=document.getElementsByTagName('body')[0]
+        if(event.target.innerText=="Red"){
+            body.style.backgroundColor='red'
+        }
+        if(event.target.innerText=="Green"){
+            body.style.backgroundColor='green'
+        }
+    }
+    //2c
+     function redirectToGoogle(){
+   window.location.assign('http://google.com')
+     }
 /* Task 4 using jQuery */
